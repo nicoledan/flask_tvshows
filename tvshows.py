f1e2d1facecd74697c8ca8254304345b901ce28d
nicoledan <nicole_k_dan@hotmail.com>
2017-03-31 19:08:02 -0400

    third commit

--- tvshows.py
+++ tvshows.py
@@ -1,39 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from emmys_data import EMMYS
 app = Flask(__name__)
 
-def get_shows(source):
-    shows = []
-    for row in source:
-        show = row["Show"]
-        shows.append([show])
-    return shows
+app.config['SECRET_KEY'] = 'abcd?efgh#-string'
 
-def get_showdata(source, show):
+def get_ids_and_shows(source):
+    ids_and_shows = []
+    for row in source:
+        id = row["ID"]
+        show = row["Show"]
+        ids_and_shows.append([id, show])
+    return ids_and_shows
+
+def get_showdata(source, id):
     for row in source:
         if id == str( row["ID"] ):
             show == row["Show"]
             network = row["Network"]
             category = row["Category"]
+            id = str(id)
     return id, show, network, category
 
 
 @app.route('/')
 @app.route('/index/')
 def index():
-    shows = get_shows(EMMYS)
-    return render_template('index.html', id=id)
+    ids_and_shows = get_ids_and_shows(EMMYS)
+    return render_template('index.html', pairs=ids_and_shows)
 
-@app.route('/index/<show>')
-def detail(show):
-    # the URL will have underscores in place of spaces - fix that
-    fixed_title = show.replace("_", " ")
-    # get variables for the book detail page: pass title with spaces restored
-    # to the function that will find that book's record
-    show, network, category = get_showdata(EMMYS, fixed_title)
-    # pass the data for the selected book to the template
-    return render_template('detail.html', id=id, show=show, network=network,
-    category=category)
+
+@app.route('/detail/<id>')
+
+def detail(id):
+    id, show, network, cateogry = get_showdata(EMMYS,id)
+    return render_template('detail.html', show=show, network=network, category=category )
 
 if __name__ == '__main__':
     app.run(debug=True)
